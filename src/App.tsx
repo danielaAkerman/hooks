93ab48150a9a06a486b2cffee075876c61d09b0c
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-04-06 23:38:49 -0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
+export function App() {
+  const [valor, setValor] = useState(0);
 
+  function modificarCount() {
+    setValor(valor + 1);
+  }
+  return <div onClick={modificarCount}>Count: {valor}</div>;
+}
