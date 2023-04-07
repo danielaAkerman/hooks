e862bdcc74c5343327345537ebd83157dd4bcbba
Daniela Akerman <daniela.akerman@mi.unc.edu.ar>
2023-04-06 23:46:16 -0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function App() {
   const [valor, setValor] = useState(0);
 
+  useEffect(() => {
+    console.log("Valor cambió", valor);
+  }, [valor]);
+
   function modificarCount() {
     setValor(valor + 1);
   }
+
   return <div onClick={modificarCount}>Count: {valor}</div>;
 }
